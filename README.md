03e923f58e48544087bfdb9c3957dfe6d66547f6
ttieman <tiemantanner@gmail.com>
2023-01-31 15:21:46 -0700

    updated the comments in code and updated read me

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # work-schedule-manager
+
+## About 
+This is a work day scheduler it will highlight sections based on what time it is in the day to help determine what task is next in your schedule
+
+## Function 
+You can edit each block with a desciption of tasks you have throughout your day.
+Then you can save that desciption by pressing the save button and it will persist when the page is reloaded.
+The gray sections are sections of time that are in the past
+The red sections are indicating the current hour tasks
+The Green sections are future scheduled tasks
+
+## ScreenShot  
+![alt = "A Screenshot of the work day scheduler"](./assets/workdayscheduler.png)
+
+## License 
+
+MIT
+
+## Citing Credit
+
+ Web3 schools was used for referencing javascript elements while building this page!
+
+ [https://www.w3schools.com/](doc:linking-to-reference#https://www.w3schools.com/)
+
+ DayJS Was used to implement the times and dates involved in this project
+
+ [https://day.js.org/](doc:linking-to-reference#https://day.js.org/)
+
+ Jquery was used to select things on screen and reference the docs for other uses.
+
+ [https://jquery.com/](doc:linking-to-reference#https://jquery.com/)
--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,15 @@
 
 $(function () {
-    var currentDay = dayjs().format("[Today is: ]dddd, MMMM D, YYYY");
-    var currentTime = dayjs().format('[Current Time is: ]h:mm A');
-    var current24HourTime = parseInt(dayjs().format("H"));
-    var save = $(".saveBtn");
-    var dayEl = $("#currentDay");
-    var textAreas = $(".description");
+    var currentDay = dayjs().format("[Today is: ]dddd, MMMM D, YYYY");              //this is the current day variable utilizing dayjs
+    var currentTime = dayjs().format('[Current Time is: ]h:mm A');              //this us the current time variable utilizing dayjs
+    var current24HourTime = parseInt(dayjs().format("H"));                 //current military time used to determine the color scheme based on time 
+    var save = $(".saveBtn");                                            //jquery selector to select the save button  
+    var dayEl = $("#currentDay");                                         // jquery selector to select the current day element 
+    var textAreas = $(".description");                                    //jquery selector to select the text areas of each time block 
     
     
     
-    function saveContent(){
+    function saveContent(){                                                 //this function saves the content to local storage when the save button is pressed 
         var textInput = $(this).closest('div').find('textarea').val();
         var textInputValue = textInput;
         var textInputId = $(this).closest("div").attr("id");
@@ -18,14 +18,14 @@ $(function () {
         return;
     }
     
-    setInterval(() => {
+    setInterval(() => {                                                           //this function updates the time every minute on the screen 
         let currentUpdateTime = dayjs().format('[Current Time is: ]h:mm A');
         dayEl.html(currentDay + "<br>" + "<br>" + currentUpdateTime); 
         console.log(currentUpdateTime);
     }, 60000);
     
     
-    $(document).ready(function() {
+    $(document).ready(function() {            // this function pulls the stored values from local storage when the page loads and siplays them on the screen 
         textAreas.each(function() {
             var textInput = $(this);
             var textInputId = textInput.closest("div").attr('id');
@@ -35,12 +35,12 @@ $(function () {
         
         dayEl.html((currentDay + "<br>" + "<br>" + currentTime));
         console.log(current24HourTime);
-        initialColors();
+        initialColors();         //this calls the initial colors funtion 
         
 })
 
-function initialColors(){
-    $('.time-block').each(function(){
+function initialColors(){                                    // this determins what the colors of each block will be based on the time when the page loads 
+    $('.time-block').each(function(){                       
         var now = dayjs().hour(current24HourTime);
       var eventTime = dayjs().hour(parseInt($(this).attr("id")));
       
@@ -54,7 +54,7 @@ function initialColors(){
 })}
 
 setInterval(() => {
-    $('.time-block').each(function(){
+    $('.time-block').each(function(){              //this will update the colors as time changes 
         var now = dayjs().hour(current24HourTime);
       var eventTime = dayjs().hour(parseInt($(this).attr("id")));
       
@@ -71,6 +71,6 @@ setInterval(() => {
   }, 60000);
 
    
-save.on("click", saveContent);
+save.on("click", saveContent);        // this is the event listener that handles listening for a click on the save button 
 
   });
